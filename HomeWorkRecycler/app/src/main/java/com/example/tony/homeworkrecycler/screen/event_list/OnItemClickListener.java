2c9c8f204b5deaf54b352794ea2162cbd2ce4904
Tynx <drozdov.anton11@gmail.com>
2017-10-09 23:56:43 +0300

    HomeWork 3,4

---
+++ HomeWorkRecycler/app/src/main/java/com/example/tony/homeworkrecycler/screen/event_list/OnItemClickListener.java
@@ -0,0 +1,9 @@
+package com.example.tony.homeworkrecycler.screen.event_list;
+
+/**
+ * Created by Tony on 25.09.2017.
+ */
+
+public interface OnItemClickListener {
+    void onClick(int position);
+}
